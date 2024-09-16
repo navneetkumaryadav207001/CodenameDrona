9ad27943bbef8171981c19a422bfbb91e683ac94
navneetkumaryadav786 <166044990+navneetkumaryadav786@users.noreply.github.com>
2024-09-17 01:04:13 +0530

    Add files via upload

---
+++ template/welcome.js
@@ -0,0 +1,5 @@
+// window.addEventListener("scroll",function(){
+//     var header= document.querySelector("header");
+//     let value = window.scrollY;
+//     header.style.marginTop = -0.04*value +'px';
+// })
